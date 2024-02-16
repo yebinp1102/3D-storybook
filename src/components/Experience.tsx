25de22889acd96976f592ffecc2db2b50528af8d
yebin <yebinp1102@naver.com>
2024-02-16 22:44:36 +0900

    Feat: adding text to the background and fix scroll effect

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,4 @@
-import { Float, Line, OrbitControls, PerspectiveCamera, useScroll } from "@react-three/drei";
+import { Float, OrbitControls, PerspectiveCamera, useScroll, Text } from "@react-three/drei";
 import Background from "./Background";
 import Airplane from "./Airplane";
 import Cloud from "./Cloud";
@@ -7,6 +7,10 @@ import { useMemo, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 
 const LINE_NB_POINTS = 12000;
+const CURVE_DISTANCE = 250;
+const CURVE_AHEAD_CAMERA = 0.008;
+const CURVE_AHEAD_AIRPLANE = 0.02;
+const AIRPLANE_MAX_ANGLE = 35;
 
 
 const Experience = () => {
@@ -17,16 +21,16 @@ const Experience = () => {
   const curve = useMemo(() => {
     return new THREE.CatmullRomCurve3([
       new THREE.Vector3(0, 0, 0),
-      new THREE.Vector3(0, 0, -10),
-      new THREE.Vector3(-2, 0, -20),
-      new THREE.Vector3(-3, 0, -30),
-      new THREE.Vector3(0, 0, -40),
-      new THREE.Vector3(5, 0, -50),
-      new THREE.Vector3(7, 0, -60),
-      new THREE.Vector3(5, 0, -70),
-      new THREE.Vector3(0, 0, -80),
-      new THREE.Vector3(0, 0, -90),
-      new THREE.Vector3(0, 0, -100),
+      new THREE.Vector3(0, 0, -CURVE_DISTANCE),
+      new THREE.Vector3(100, 0, -2 * CURVE_DISTANCE),
+      new THREE.Vector3(-100, 0, -3 * CURVE_DISTANCE),
+      new THREE.Vector3(100, 0, -4 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -5 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -6 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -7 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -8 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -9 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -10 * CURVE_DISTANCE),
     ],
     false,
     "catmullrom",
@@ -47,53 +51,81 @@ const Experience = () => {
   }, [curve]);
 
   useFrame((_state, delta) => {
-    const curPointIndex = Math.min(
-      Math.round(scroll.offset * linePoints.length),
-      linePoints.length - 1
+    const scrollOffset = Math.max(0, scroll.offset);
+
+    const curPoint = curve.getPoint(scrollOffset);
+
+    // Follow the curve points
+    cameraGroup.current.position.lerp(curPoint, delta * 24);
+
+    // Make the group look ahead on the curve
+
+    const lookAtPoint = curve.getPoint(
+      Math.min(scrollOffset + CURVE_AHEAD_CAMERA, 1)
     );
-    const curPoint = linePoints[curPointIndex];
-    const pointAhead =
-      linePoints[Math.min(curPointIndex + 1, linePoints.length - 1)];
 
-    const xDisplacement = (pointAhead.x - curPoint.x) * 80;
+    const currentLookAt = cameraGroup.current.getWorldDirection(
+      new THREE.Vector3()
+    );
+    const targetLookAt = new THREE.Vector3()
+      .subVectors(curPoint, lookAtPoint)
+      .normalize();
 
-    // Math.PI / 2 -> LEFT
-    // -Math.PI / 2 -> RIGHT
+    const lookAt = currentLookAt.lerp(targetLookAt, delta * 24);
+    cameraGroup.current.lookAt(
+      cameraGroup.current.position.clone().add(lookAt)
+    );
 
-    const angleRotation =
-      (xDisplacement < 0 ? 1 : -1) *
-      Math.min(Math.abs(xDisplacement), Math.PI / 3);
+    // Airplane rotation
+
+    const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
+
+    const nonLerpLookAt = new THREE.Group();
+    nonLerpLookAt.position.copy(curPoint);
+    nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
+
+    tangent.applyAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      -nonLerpLookAt.rotation.y
+    );
+
+    let angle = Math.atan2(-tangent.z, tangent.x);
+    angle = -Math.PI / 2 + angle;
+
+    let angleDegrees = (angle * 180) / Math.PI;
+    angleDegrees *= 2.4; // stronger angle
+
+    // LIMIT PLANE ANGLE
+    if (angleDegrees < 0) {
+      angleDegrees = Math.max(angleDegrees, -AIRPLANE_MAX_ANGLE);
+    }
+    if (angleDegrees > 0) {
+      angleDegrees = Math.min(angleDegrees, AIRPLANE_MAX_ANGLE);
+    }
+
+    // SET BACK ANGLE
+    angle = (angleDegrees * Math.PI) / 180;
 
     const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
       new THREE.Euler(
         airplane.current.rotation.x,
         airplane.current.rotation.y,
-        angleRotation
+        angle
       )
     );
-    const targetCameraQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        cameraGroup.current.rotation.x,
-        angleRotation,
-        cameraGroup.current.rotation.z
-      )
-    );
-
     airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
-    cameraGroup.current.quaternion.slerp(targetCameraQuaternion, delta * 2);
-
-    cameraGroup.current.position.lerp(curPoint, delta * 24);
   });
 
 
   return (
     <>
-      {/* <OrbitControls enableZoom={false} /> */}
+      {/* <OrbitControls /> */}
+      {/* airplane model */}
       <group ref={cameraGroup}>
         <Background />
         <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault/>
         <group ref={airplane}>
-          <Float floatIntensity={2} speed={2}>
+          <Float floatIntensity={1} speed={1.5} rotationIntensity={0.5}>
             <Airplane 
               rotation-y={Math.PI / 2}
               scale={[0.2, 0.2, 0.2]}
@@ -101,6 +133,49 @@ const Experience = () => {
             />
           </Float>
         </group>
+      </group>
+
+      {/* text */}
+      <group position={[-3, 0, -10]}>
+        <Text
+          color="black"
+          anchorX={"left"}
+          anchorY="middle"
+          fontSize={0.2}
+          maxWidth={3.5}
+          lineHeight={1.2}
+          font={"./fonts/NanumGothic-ExtraBold.ttf"}
+        >
+          안녕하세요! {"\n"}
+          지금부터 함께 신나는 모험을 떠나볼까요?
+        </Text>
+      </group>
+
+      <group position={[1, 0, -40]}>
+        <Text
+          color="white"
+          anchorX={"left"}
+          anchorY="middle"
+          fontSize={0.26}
+          maxWidth={3.5}
+          lineHeight={1.2}
+          font={"./fonts/NanumGothic-ExtraBold.ttf"}
+        >
+          아차! 제 소개를 먼저 해드릴게요. {"\n"}
+        </Text>
+        <Text
+          color="white"
+          anchorX={"left"}
+          anchorY="top"
+          position-y={-0.25}
+          fontSize={0.15}
+          lineHeight={1.3}
+          maxWidth={3.5}
+          font={"./fonts/NanumGothic-Regular.ttf"}
+        >
+          전 이번에 뭉게항공에 입사하게 된 비행기라고해요.
+          아버지의 꿈을 이어 여객기가 되었답니다!
+        </Text>
       </group>
 
       {/* Line */}
@@ -121,22 +196,23 @@ const Experience = () => {
       </group>
 
       {/* CLOUDS */}
-      <Cloud opacity={0.5} scale={[0.3, 0.3, 0.3]} position={[-2, 1, -3]} />
-      <Cloud opacity={0.5} scale={[0.2, 0.3, 0.4]} position={[1.5, -0.5, -2]} />
-      <Cloud
-        opacity={0.7}
-        scale={[0.3, 0.3, 0.4]}
-        rotation-y={Math.PI / 9}
-        position={[2, -0.2, -2]}
+      <Cloud opacity={0.6} scale={[1, 1, 1.5]} position={[-3.5, -1.2, -7]} />
+      <Cloud opacity={0.6} scale={[1, 1, 2]} position={[3.5, -1, -10]} rotation-y={Math.PI} />
+      <Cloud 
+        opacity={0.9}
+        scale={[1, 1, 1]}
+        position={[-3.5, 0.2, -12]}
+        rotation-y={Math.PI / 3}
       />
-      <Cloud
-        opacity={0.7}
+      <Cloud opacity={0.9} scale={[1, 1, 1]} position={[3.5, 0.2, -12]} />
+
+      <Cloud opacity={0.4}
         scale={[0.4, 0.4, 0.4]}
         rotation-y={Math.PI / 9}
         position={[1, -0.2, -12]}
       />
-      <Cloud opacity={0.7} scale={[0.5, 0.5, 0.5]} position={[-1, 1, -53]} />
-      <Cloud opacity={0.3} scale={[0.8, 0.8, 0.8]} position={[0, 1, -100]} />
+      <Cloud opacity={0.6} scale={[0.3, 0.5, 2]} position={[-4, -0.5, -53]} />
+      <Cloud opacity={0.7} scale={[0.8, 0.8, 0.8]} position={[-1, -1.5, -100]} />
     </>
   )
 }
--- src/pages/ExploreProjects.tsx
+++ src/pages/ExploreProjects.tsx
@@ -7,7 +7,7 @@ const ExploreProjects = () => {
     <div className='w-screen h-screen'>
       <Canvas>
         <color attach="background" args={['#ececec']} />
-        <ScrollControls pages={5} damping={0.3}>
+        <ScrollControls pages={100} damping={1}>
           <Experience />
         </ScrollControls>
       </Canvas>
