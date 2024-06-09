5008be8b421e596e1242bfe0f4db4de10f96a1f9
yebin <yebinp1102@naver.com>
2024-06-09 12:12:14 +0900

    Fix: client axios baseUrl update

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.PROD ? '' : 'http://localhost:3000',
+  baseURL: "3d-storybook-api.vercel.app",
 })
 
 
